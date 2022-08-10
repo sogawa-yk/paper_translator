172ee1d7242f8af749130c0a1775fd5f3b9d2e4f
曽川宥輝 <sogawa@sogawanoMacBook-Air.local>
2022-08-10 15:51:20 +0900

    PDF表示と翻訳結果表示の外観ができた

--- lib/main.dart
+++ lib/main.dart
@@ -5,4 +5,17 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Upload pdf file'),
+    );
+  }
+}
